dfb67dfa965374c76f73ee61376157c16565c3aa
Chidi Emeh <chidiemeh184@gmail.com>
2018-08-05 14:48:02 -0400

    Test firebase database

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //
 //  app.js
-//  TravelistoV2_Server
+//  TravelistoV2-API
 //
 //  Created by Chidi Emeh on 8/5/18.
 //  Copyright © 2018 Chidi Emeh. All rights reserved.
@@ -9,6 +9,34 @@
 // Require express and create an instance of it
 var express = require('express');
 var app = express();
+const config = require('./config.json');
+
+//Firebase
+var admin = require('firebase-admin');
+
+//Firebase
+var firebase = require('firebase/app');
+require('firebase/auth');
+require('firebase/database');
+
+firebase.initializeApp({
+    serviceAccount: "./service-account.json",
+    databaseURL: config.databaseURL
+});
+
+var ref = firebase.database().ref()
+
+ref.once('value')
+    .then(function(snap){
+        console.log('Snapvalue : ', snap.val());
+    });
+
+ref.child('users/').set({
+    username: "user1",
+    email: "user1@gmail.com",
+    age: "27"
+});
+
 
 // on the request to root (localhost:3000/)
 app.get('/v1/travelisto/home', function (req, res) {
